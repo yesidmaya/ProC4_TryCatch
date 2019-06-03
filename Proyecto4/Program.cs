a8464b3c9ae28f86ba5b4cc9a1501b9ae783c5c5
Alejandro <yesidmaya@gmail.com>
2019-06-03 15:34:35 -0500

    TryCatch

---
+++ Proyecto4/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Proyecto2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("INTRODUZCA UN VALOR");
+
+            try {
+                int c = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Se pulso un numero");
+            }
+            catch (Exception) {
+                Console.WriteLine("INTRODUSCA UN NUMERO POR FAVOR....");
+            }
+            finally {
+                Console.WriteLine("Siempre se ejecuta.......");
+            }
+            //if (a == 0 && b > 3) // AND
+            //{
+            //    Console.WriteLine("hola");
+            //}
+            //else {
+            //    Console.WriteLine("ADIOS");
+            //}
+
+            //if (a == 0 || b > 3) // OR
+            //{
+            //    Console.WriteLine("hola");
+            //}
+            //else {
+            //    Console.WriteLine("ADIOS");
+            //}
+
+
+            Console.WriteLine("Pulse un tecla para finalizar");
+            Console.ReadLine();
+        }
+    }
+}
